4f45f623c5471773045b2fd7bea7ea1a64199d4d
Andy Wardley <abw@wardley.org>
2023-03-10 13:47:40 +0000

    mergeSets()

--- test/merge_sets.js
+++ test/merge_sets.js
@@ -1,17 +1,80 @@
 import test from 'ava';
-import mergeSelectedIconSets from '../lib/mergeSets.js';
+import { mergeIconSets, mergeSelectedIconSets } from '../lib/mergeSets.js';
 
+//--------------------------------------------------------------------------
+// mergeIconSets()
+//--------------------------------------------------------------------------
 const sets1 = {
+  solid: {
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+  },
+  regular: {
+    square: 'square1'
+  },
+  just1: {
+    one: 'one'
+  }
+}
+const sets2 = {
+  solid: {
+    wig: 'wig',
+    wam: 'wam',
+    bam: 'bam'
+  },
+  regular: {
+    square: 'square2'
+  },
+  just2: {
+    two: 'two'
+  }
+}
+
+const setsExpect = {
+  solid: {
+    foo: 'foo',
+    bar: 'bar',
+    baz: 'baz',
+    wig: 'wig',
+    wam: 'wam',
+    bam: 'bam'
+  },
+  regular: {
+    square: 'square2'   // sets2 version replaces sets1
+  },
+  just1: {
+    one: 'one'
+  },
+  just2: {
+    two: 'two'
+  }
+}
+
+test(
+  'mergeIconSets',
+  t => t.deepEqual(
+    mergeIconSets(sets1, sets2),
+    setsExpect
+  )
+);
+
+
+//--------------------------------------------------------------------------
+// mergeSelectedIconSets()
+//--------------------------------------------------------------------------
+
+const selected1 = {
   foo: ['foo1', 'foo2'],
   bar: ['bar1', 'bar2']
 }
 
-const sets2 = {
+const selected2 = {
   foo: ['foo3'],
   baz: ['baz1']
 }
 
-const expect = {
+const selectedExpect = {
   foo: ['foo1', 'foo2', 'foo3'],
   bar: ['bar1', 'bar2'],
   baz: ['baz1']
@@ -19,5 +82,8 @@ const expect = {
 
 test(
   'mergeSelectedIconSets',
-  t => t.deepEqual(mergeSelectedIconSets(sets1, sets2), expect)
+  t => t.deepEqual(
+    mergeSelectedIconSets(selected1, selected2),
+    selectedExpect
+  )
 );
